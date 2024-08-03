c6f1e2cc40d2487e12b7da0c124d045a9e30e5e4
Brian Dam Pedersen <brian.dam.pedersen@gmail.com>
2024-08-03 23:23:16 +0200

    Support for TFUNCTION as anonymous callable objects

---
+++ classes/LuaContext+AnonCall.h
@@ -0,0 +1,13 @@
+//
+//  LuaContext+AnonCall.h
+//  LuaTests
+//
+//  Created by Brian Pedersen on 03/08/2024.
+//  Copyright © 2024 Sean Meiners. All rights reserved.
+//
+
+#ifndef LuaContext_AnonCall_h
+#define LuaContext_AnonCall_h
+
+
+#endif /* LuaContext_AnonCall_h */
